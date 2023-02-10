a44d05677c5f99ba231e5ca3bd4e6006e3e757f7
shady-mungai <shadrackmungai10@gmail.com>
2023-02-10 22:17:39 +0300

    Initialized countdown program

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
+#require 'pry'
 
-def countdown
+def countdown num
+    if num > 0
+        while num >= 1
+            num -= 1
+            #binding.pry
+            puts "#{num} SECOND(S)!"
+        end
+        
+    end
+    puts "HAPPY NEW YEAR!"
+    else 
+        puts "Can only count down an integer"
+end
+
+def countdown_with_sleep int
 end
